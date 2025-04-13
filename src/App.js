41e9d5f6b4d65ca59ee1a6ce522ad84b4ce51c74
Arvind Reddy Bobbili <arvindrv2@gmail.com>
2025-04-13 18:11:06 -0500

    edited_App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // src/App.js
 import React from "react";
+import { DarkModeProvider } from "./context/DarkModeContext";
+import { ScrollProvider } from "./context/ScrollContext";
 import Navbar from "./components/Navbar"; // Import the Navbar component
 import Home from "./components/Home"; // Import the Home component
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Navbar /> {/* Render the Navbar */}
-      <Home /> {/* Render the Home component */}
-    </div>
+    <DarkModeProvider>
+      <ScrollProvider>
+        <div className="App">
+          <Navbar /> {/* Render the Navbar */}
+          <Home /> {/* Render the Home component */}
+        </div>
+      </ScrollProvider>
+    </DarkModeProvider>
   );
 }
 
